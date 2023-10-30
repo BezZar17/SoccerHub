c7d98548b5734fa4ae14cd4292dcc77846e82a7f
Cesar BezZar17 <ccruz2408@outlook.com>
2023-10-29 23:35:45 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <Hero />
+            <Demo />
+        </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <header>
+        <nav>
+            <div className='webName'>Soccer Hub</div>
+            <button type='button' onClick={() => window.open('')}>Sign Up</button>
+        </nav>
+        <h1>Today's Matches</h1>
+        <h2>Click on a league to see today's scores</h2>
+
+    </header>
+  )
+}
+
+export default Hero
